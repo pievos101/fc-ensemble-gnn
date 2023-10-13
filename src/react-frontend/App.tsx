8459705569d506b8bcb5484cf783c01a89fad7bf
ChristianMatthias <h.christian966@gmail.com>
2023-10-13 14:05:28 +0200

    added error handler

--- src/react-frontend/App.tsx
+++ src/react-frontend/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createTheme, ThemeProvider} from "@mui/material";
+import { Alert, AlertTitle, Button, createTheme, ThemeProvider, useTheme } from "@mui/material";
 import {MainContainer} from "./components/MainContainer";
 import {ContentContainer} from "./components/ContentContainer";
 import {SidePanel} from "./components/side-panel/SidePanel";
@@ -8,6 +8,7 @@ import {
     QueryClientProvider,
 } from '@tanstack/react-query'
 import {TestSetProvider} from "./queries/useTestSet";
+import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient()
 
@@ -15,10 +16,31 @@ export function getApiUrl() {
     return window.location.href + '/api'
 }
 
+function ErrorFallback({ error, resetErrorBoundary }: any) {
+  return (
+    <div style={{
+      display: "flex",
+      width: "100vw",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Alert severity={"error"}>
+        <AlertTitle>A React runtime error occured</AlertTitle>
+        Error: <pre>{error.message}</pre>
+        <Button onClick={resetErrorBoundary} variant={"contained"} color={"error"}>
+          Try again
+        </Button>
+      </Alert>
+    </div>
+  );
+}
+
 function App() {
     const theme = createTheme({});
 
     return (
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <ThemeProvider theme={theme}>
             <QueryClientProvider client={queryClient}>
                 <TestSetProvider>
@@ -29,6 +51,7 @@ function App() {
                 </TestSetProvider>
             </QueryClientProvider>
         </ThemeProvider>
+      </ErrorBoundary>
     );
 }
 
