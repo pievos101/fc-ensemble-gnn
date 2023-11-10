8744359d1035f5f9316eeb6aa954f1b4c9c961a4
ChristianMatthias <h.christian966@gmail.com>
2023-11-10 11:28:35 +0100

    improved local dev server

--- src/local_main.py
+++ src/local_main.py
@@ -6,18 +6,15 @@ from local_run import runWorkflow
 from web import applyWebServerRoutes
 
 client = Client()
-global_client = Client()
 server = Bottle()
 server.install(cors_plugin('*'))
 
 # add the api implementation
-applyWebServerRoutes(server, client, global_client, None)
+applyWebServerRoutes(server, client, client, None)
 
 # run the training workflow to get the local model
-runWorkflow(client)
-global_client.ensemble = client.ensemble
-global_client.training_complete = True
+runWorkflow(client, "data/client1/synthetic")
 
 # start the server
 server.mount('/web-api', server)
-server.run(host='localhost', port=5001, debug=True)
+server.run(host='localhost', port=5001, debug=True)  # reloader=True for hot reloads... but always retrains the model
--- src/local_run.py
+++ src/local_run.py
@@ -1,28 +1,34 @@
-from algo import Client, getGraphs
+import os
+
+import bios
+
+from algo import Client
 
 
-def runWorkflow(external_client: Client = None):
+def runWorkflow(external_client: Client = None, data_location: str = "data/synthetic"):
     client = external_client
     if client is None:
         client = Client()
     print("starting script")
-    # location of the files
-    loc = "data/synthetic"
-    # PPI network
-    ppi = f'{loc}/NETWORK_synthetic.txt'
-    # single-omic features
-    feats = [f'{loc}/FEATURES_synthetic.txt']
-    # multi-omic features
-    # feats = [f'{loc}/KIDNEY_RANDOM_mRNA_FEATURES.txt', f'{loc}/KIDNEY_RANDOM_Methy_FEATURES.txt']
-    # outcome class
-    targ = f'{loc}/TARGET_synthetic.txt'
-    client.readInputDataAndSetupSubNet(loc, ppi, feats, targ)
+    # read config
+    config = bios.read(os.path.join(data_location, 'config.yml'))
+
+    # get input file name from config
+    input_features_name = config['input_features_name']
+    input_ppi_name = config['input_ppi_name']
+    input_target_name = config['input_target_name']
+    # wait_for_ui = config['wait_for_ui'] - is disregarded as there is no point in running the app without the UI
+
+    # generate file paths
+    ppi = os.path.join(data_location, input_ppi_name)
+    feats = os.path.join(data_location, input_features_name)
+    targ = os.path.join(data_location, input_target_name)
+
+    client.readInputDataAndSetupSubNet(data_location, ppi, [feats], targ)
     client.splitSubNetIntoTrainAndTest()
     client.train()
     client.checkValidationSetPerformance()
     client.checkTestSetPerformance()
-    print("printing graph")
-    print(getGraphs(client.ensemble)[0])
 
 
 if __name__ == '__main__':
